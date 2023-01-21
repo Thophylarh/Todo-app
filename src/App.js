da75f3505d5e7f61fe73c99ae7fe2062586db739
Thophylarh <iwualathophylarh@gmail.com>
2023-01-21 19:25:24 +0100

    Add todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import "./App.css";
+import Form from './component/Form';
+import Header from "./component/Header";
+import TodoList from './component/TodoList';
+const App = () => {
+const [input, setInput]=useState("");
+const [todos, setTodos]= useState([]);
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form 
+          input={input} setInput={setInput}
+todos={todos} setTodos={setTodos}
+          />
+        </div>
+        <div>
+          <TodoList todos={todos} setTodos={setTodos} />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
